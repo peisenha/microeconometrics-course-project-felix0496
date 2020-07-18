143b0808665eb25610acb6e687c1a3d9ad9c1310
felix0496 <40427105+felix0496@users.noreply.github.com>
2020-07-18 22:26:30 +0200

    Update travis_runner.py

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -6,5 +6,5 @@ import glob
 if __name__ == '__main__':
 
     for notebook in glob.glob('*.ipynb'):
-        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=300'.format(notebook)
         sp.check_call(cmd, shell=True)
